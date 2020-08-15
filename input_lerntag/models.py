fb3c9f581d276f287809b4bd66dec0e08bfaac51
AlexKnowsIt <ali.green@web.de>
2020-08-15 12:37:27 +0200

    django way of adding current date

--- input_lerntag/models.py
+++ input_lerntag/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 from datetime import date
+from django.utils.timezone import now
 # Create your models here.
 
 class Lerntag (models.Model):
-    Date = date.today()
-    datum = models.DateField(default=Date)
+    datum = models.DateField(default=now)
     notiz = models.TextField(blank=True, null=True)
     zeit_lernen = models.DecimalField(decimal_places=2, max_digits=4)
     zeit_freizeit = models.DecimalField(decimal_places=2, max_digits=4)
