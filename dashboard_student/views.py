64803bde1cc757f3834a7ebe11a9ab3a4583b0a7
AlexKnowsIt <AlexKnowsIt@users.noreply.github.com>
2020-09-07 11:52:48 +0200

    added views for dashboard

--- dashboard_student/views.py
+++ dashboard_student/views.py
@@ -5,3 +5,46 @@ from django.http import HttpResponse
 
 def dashboard_data_view(request):
     return render (request, 'dashboard.html')
+
+# 2 oder 3 Seiten --> täglich, wöchentlich, Trend
+
+
+# täglich
+
+# Kuchendiagramm mit rel. und absoluten Werten für alle Kategorien
+def zeiteinsatz_tag_view(request): 
+    pass
+
+# Barchart Ist Soll
+def zeiteinsatz_tag_istvssoll_view(request):
+    pass
+
+
+# wöchentlich
+
+# Liniendiagramm Arbeit (DW SW) im Zeitverlauf
+def zeiteinsatz_woche_arbeit_view(request):
+    pass
+
+# Kuchendiagramm mit rel. und absoluten Werten für alle Kategorien
+def zeiteinsatz_woche_view(request):
+    pass
+
+
+# Trend
+
+# letzte 30 Tage Arbeit vs 30 Tage davor
+def zeiteinsatz_monat_arbeit_view(request):
+    pass
+
+# Zeiteinsatz über Monat als Kuchendiagramm
+def zeiteinsatz_monat_view(request):
+    pass
+
+# Produktivität, Wohlbefinden und Deepwork Zeit
+def zeiteinsatz_monat_combined_productivity_view(request):
+    pass
+
+# Liniendiagramm alle Kategorien über Monat
+def zeiteinsatz_monat_all_cat_view(request):
+    pass
