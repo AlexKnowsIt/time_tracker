460d2c416e1ddc8c6a11195800607d6246f4de74
AlexKnowsIt <AlexKnowsIt@users.noreply.github.com>
2020-09-07 11:30:28 +0200

    added defaults and registered

--- input_lerntag/admin.py
+++ input_lerntag/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from.models import Lerntag
 # Register your models here.
+
+admin.site.register(Lerntag)
--- input_lerntag/models.py
+++ input_lerntag/models.py
@@ -5,7 +5,7 @@ from django.utils.timezone import now
 
 class Lerntag (models.Model):
     datum = models.DateField(default=now)
-    zeit_arbeit_mental = models.DecimalField(decimal_places=1, max_digits=3)
-    zeit_arbeit_shallow = models.DecimalField(decimal_places=1, max_digits=3)
-    zeit_freizeit = models.DecimalField(decimal_places=1, max_digits=3)
-    zeit_organisation = models.DecimalField(decimal_places=1, max_digits=3)
+    zeit_arbeit_mental = models.DecimalField(decimal_places=1, max_digits=3, default=0)
+    zeit_arbeit_shallow = models.DecimalField(decimal_places=1, max_digits=3, default=0)
+    zeit_freizeit = models.DecimalField(decimal_places=1, max_digits=3, default=0)
+    zeit_organisation = models.DecimalField(decimal_places=1, max_digits=3, default=0)
