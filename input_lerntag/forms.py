83342cae51593b7962202b8505748259cb1cb36b
AlexKnowsIt <AlexKnowsIt@users.noreply.github.com>
2020-09-11 01:01:31 +0200

    enhanced model outcome

--- input_lerntag/forms.py
+++ input_lerntag/forms.py
@@ -2,6 +2,16 @@ from django import forms
 from .models import Lerntag
 
 class LerntagForm (forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['datum'].widget.attrs.update({'class': 'form-control form-control-sm m-1 bg-dark text-light'})
+        self.fields['zeit_arbeit_mental'].widget.attrs.update({'class': 'form-control form-control-sm m-1 bg-dark bg-dark text-light', 'placeholder': 'in Stunden'})
+        self.fields['zeit_arbeit_shallow'].widget.attrs.update({'class': 'form-control form-control-sm m-1 bg-dark text-light', 'placeholder': 'in Stunden'})
+        self.fields['zeit_freizeit'].widget.attrs.update({'class': 'form-control form-control-sm m-1 bg-dark text-light', 'placeholder': 'in Stunden'})
+        self.fields['zeit_organisation'].widget.attrs.update({'class': 'form-control form-control-sm m-1 bg-dark text-light', 'placeholder': 'in Stunden'})
+        self.fields['output_productivity'].widget.attrs.update({'class': 'form-control form-control-sm m-1 border-success bg-dark text-light', 'placeholder': 'von 1 bis 10', 'max': 10})
+        self.fields['output_happiness'].widget.attrs.update({'class': 'form-control form-control-sm m-1 bg-dark border-success text-light', 'placeholder': 'von 1 bis 10', 'max': 10})
+
     class Meta:
         model = Lerntag
         fields = [
@@ -9,5 +19,7 @@ class LerntagForm (forms.ModelForm):
             'zeit_arbeit_mental',
             'zeit_arbeit_shallow', 
             'zeit_freizeit',
-            'zeit_organisation'
+            'zeit_organisation',
+            'output_productivity',
+            'output_happiness'
         ]
--- input_lerntag/models.py
+++ input_lerntag/models.py
@@ -1,11 +1,15 @@
 from django.db import models
 from datetime import date
 from django.utils.timezone import now
+from django.contrib.auth.models import User
 # Create your models here.
 
 class Lerntag (models.Model):
     datum = models.DateField(default=now)
-    zeit_arbeit_mental = models.DecimalField(decimal_places=1, max_digits=3, default=0)
-    zeit_arbeit_shallow = models.DecimalField(decimal_places=1, max_digits=3, default=0)
-    zeit_freizeit = models.DecimalField(decimal_places=1, max_digits=3, default=0)
-    zeit_organisation = models.DecimalField(decimal_places=1, max_digits=3, default=0)
+    zeit_arbeit_mental = models.DecimalField(decimal_places=1, max_digits=3, null=False, blank=False)
+    zeit_arbeit_shallow = models.DecimalField(decimal_places=1, max_digits=3, null=False, blank=False)
+    zeit_freizeit = models.DecimalField(decimal_places=1, max_digits=3, null=False, blank=False)
+    zeit_organisation = models.DecimalField(decimal_places=1, max_digits=3, null=False, blank=False)
+    output_productivity = models.PositiveIntegerField(null=True, blank=False)
+    output_happiness = models.PositiveIntegerField(null=True, blank=False)
+    student = models.ForeignKey(User, on_delete=models.CASCADE, blank=True)
